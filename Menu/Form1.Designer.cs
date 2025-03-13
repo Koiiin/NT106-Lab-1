2e3d4bafab13e1892de5643553e5b21b8735e11e
minuHacker <77044956+minhmap123@users.noreply.github.com>
2025-03-13 23:43:32 +0700

    fix unlimit pop-up windows

--- Menu/Form1.Designer.cs
+++ Menu/Form1.Designer.cs
@@ -38,9 +38,10 @@
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(48, 197);
+            this.button1.Location = new System.Drawing.Point(32, 126);
+            this.button1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(133, 65);
+            this.button1.Size = new System.Drawing.Size(89, 42);
             this.button1.TabIndex = 0;
             this.button1.Text = "Bài 1";
             this.button1.UseVisualStyleBackColor = true;
@@ -48,9 +49,10 @@
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(48, 349);
+            this.button2.Location = new System.Drawing.Point(32, 223);
+            this.button2.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(133, 65);
+            this.button2.Size = new System.Drawing.Size(89, 42);
             this.button2.TabIndex = 1;
             this.button2.Text = "Bài 2";
             this.button2.UseVisualStyleBackColor = true;
@@ -58,9 +60,10 @@
             // 
             // button3
             // 
-            this.button3.Location = new System.Drawing.Point(428, 197);
+            this.button3.Location = new System.Drawing.Point(285, 126);
+            this.button3.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(133, 65);
+            this.button3.Size = new System.Drawing.Size(89, 42);
             this.button3.TabIndex = 2;
             this.button3.Text = "Bài 3";
             this.button3.UseVisualStyleBackColor = true;
@@ -68,9 +71,10 @@
             // 
             // button4
             // 
-            this.button4.Location = new System.Drawing.Point(428, 349);
+            this.button4.Location = new System.Drawing.Point(285, 223);
+            this.button4.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.button4.Name = "button4";
-            this.button4.Size = new System.Drawing.Size(133, 65);
+            this.button4.Size = new System.Drawing.Size(89, 42);
             this.button4.TabIndex = 3;
             this.button4.Text = "Bài 4";
             this.button4.UseVisualStyleBackColor = true;
@@ -78,9 +82,10 @@
             // 
             // button5
             // 
-            this.button5.Location = new System.Drawing.Point(781, 197);
+            this.button5.Location = new System.Drawing.Point(521, 126);
+            this.button5.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.button5.Name = "button5";
-            this.button5.Size = new System.Drawing.Size(133, 65);
+            this.button5.Size = new System.Drawing.Size(89, 42);
             this.button5.TabIndex = 4;
             this.button5.Text = "Bài 5";
             this.button5.UseVisualStyleBackColor = true;
@@ -90,25 +95,28 @@
             // 
             this.textBox1.BackColor = System.Drawing.SystemColors.Menu;
             this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 19.875F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.textBox1.Location = new System.Drawing.Point(256, 58);
+            this.textBox1.Location = new System.Drawing.Point(171, 37);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(579, 67);
+            this.textBox1.Size = new System.Drawing.Size(387, 45);
             this.textBox1.TabIndex = 5;
             this.textBox1.Text = "Danh Sách Bài Tập ";
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1024, 539);
+            this.ClientSize = new System.Drawing.Size(683, 345);
             this.Controls.Add(this.textBox1);
             this.Controls.Add(this.button5);
             this.Controls.Add(this.button4);
             this.Controls.Add(this.button3);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.Name = "Form1";
-            this.Text = "Form1";
+            this.Text = "Menu Homework";
+            this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
             this.PerformLayout();
 
--- Menu/Form1.cs
+++ Menu/Form1.cs
@@ -17,6 +17,12 @@ namespace Menu
 {
     public partial class Form1 : Form
     {
+        private Lab1_bai1.Form1 form_bai_1;
+        private Bai_2.Form1 form_bai_2;
+        private Bai_3.output form_bai_3;
+        private Lab1_bai4.Form1 form_bai_4;
+        private Lab1_bai5.Form1 form_bai_5;
+
         public Form1()
         {
             InitializeComponent();
@@ -24,33 +30,73 @@ namespace Menu
 
         private void button1_Click(object sender, EventArgs e)
         {
-            Lab1_bai1.Form1 form1 = new Lab1_bai1.Form1();
-            form1.Show();
+            // Kiểm tra xem form1 đã được mở chưa
+            if (form_bai_1 == null || form_bai_1.IsDisposed)
+            {
+                form_bai_1 = new Lab1_bai1.Form1();
+                form_bai_1.Show();
+            }
+            else
+            {
+                form_bai_1.BringToFront(); // Đưa form lên trước nếu đã mở
+            }
         }
 
         private void button2_Click(object sender, EventArgs e)
         {
-            Form2 form2 = new Form2();
-            form2.Show();
+            if (form_bai_2 == null || form_bai_2.IsDisposed)
+            {
+                form_bai_2 = new Bai_2.Form1();
+                form_bai_2.Show();
+            }
+            else
+            {
+                form_bai_2.BringToFront();
+            }
         }
 
         private void button3_Click_1(object sender, EventArgs e)
         {
-            Bai_3.output form3 = new Bai_3.output();
-            form3.Show();
+            if (form_bai_3 == null || form_bai_3.IsDisposed)
+            {
+                form_bai_3 = new Bai_3.output();
+                form_bai_3.Show();
+            }
+            else
+            {
+                form_bai_3.BringToFront();
+            }
         }
 
         private void button4_Click(object sender, EventArgs e)
         {
-            Lab1_bai4.Form1 form4 = new Lab1_bai4.Form1();
-            form4.Show();
+            if (form_bai_4 == null || form_bai_4.IsDisposed)
+            {
+                form_bai_4 = new Lab1_bai4.Form1();
+                form_bai_4.Show();
+            }
+            else
+            {
+                form_bai_4.BringToFront();
+            }
         }
 
         private void button5_Click(object sender, EventArgs e)
         {
-            Lab1_bai5.Form1 form5 = new Lab1_bai5.Form1();
-            form5.Show();
+            if (form_bai_5 == null || form_bai_5.IsDisposed)
+            {
+                form_bai_5 = new Lab1_bai5.Form1();
+                form_bai_5.Show();
+            }
+            else
+            {
+                form_bai_5.BringToFront();
+            }
         }
 
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
     }
 }
