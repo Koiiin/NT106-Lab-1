a8b5f15ef16c15e5b160ae6df449950fa16060e2
Vh.Koiiin <145177114+Koiiin@users.noreply.github.com>
2025-03-13 19:08:15 +0700

    Add files via upload

---
+++ Menu/Form1.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Lab1_bai1;
+using Bai_2;
+using Bai_3;
+using Lab1_bai4;
+using Lab1_bai5;
+
+namespace Menu
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Lab1_bai1.Form1 form1 = new Lab1_bai1.Form1();
+            form1.Show();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Form2 form2 = new Form2();
+            form2.Show();
+        }
+
+        private void button3_Click_1(object sender, EventArgs e)
+        {
+            Bai_3.output form3 = new Bai_3.output();
+            form3.Show();
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            Lab1_bai4.Form1 form4 = new Lab1_bai4.Form1();
+            form4.Show();
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            Lab1_bai5.Form1 form5 = new Lab1_bai5.Form1();
+            form5.Show();
+        }
+
+    }
+}
